1bec89871603bdfeaa47cd787a3be7f4fb760651
Chris Underwood <cunderwoodmn@gmail.com>
2016-10-17 21:55:30 -0700

    add password length test on focus and keyup

--- js/app.js
+++ js/app.js
@@ -2,11 +2,22 @@
 //Solution: Hide and show them at appropriate times
 
 //hide Hints
+$("form span").hide();
+
+function passwordEvent(){
+    //Find out if password is valid
+    if($(this).val().length > 8) {
+      //Hide hint if valid
+      $(this).next().hide();
+    } else {
+      //else show hint
+      $(this).next().show();
+    }
+}
 
 //when even happens on password input
-//if password is valid
-  //hide hint if vaild
-  //else show hint
+$('#password').focus(passwordEvent).keyup(passwordEvent);
+
 
 //When event happens on confirmation input
   //find out if password and confirmation matcn
